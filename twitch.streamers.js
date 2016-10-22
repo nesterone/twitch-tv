ae0307bc39261fb436a9cf629356ec9900f41fe7
nesterone <igornester@gmail.com>
2016-10-22 13:15:14 +0300

    -add ajax request

--- twitch.streamers.js
+++ twitch.streamers.js
@@ -1,1 +1,31 @@
-console.log("test");
+(function (options) {
+
+    options = options || {};
+
+    var clientId = options.clientId || "";
+
+    function request(url) {
+        $.ajax({
+            url : url,
+            data: {
+                "client_id" : clientId
+            },
+            dataType: "jsonp"
+        })
+        .then(
+            function fullfill(response) {
+                console.log(response);
+            },
+            function reject(err) {
+                console.error(err);
+            }
+        )
+    }
+
+
+    request("https://api.twitch.tv/kraken/streams/freecodecamp");
+
+
+})({
+    clientId : "1v915rwzbemit2dh9i8k1kmc3sdb1rd"
+});
